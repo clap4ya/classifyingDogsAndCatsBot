342c4e03ca5b755a7f798692c11006d88600bd32
1040151 <yeolpyeong@gmail.com>
2017-07-03 16:33:31 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -2,6 +2,10 @@ from microsoftbotframework import ReplyToActivity
 import requests
 import json
 import datetime
+import random
+
+greetings = [ "hi", "hello", "hey", "yo", "greetings" ]
+greetings_responses = [ "Hi there." , "Greetings man.", "Hello there.", "Hey." ]
 
 def response(message):
   if message["type"] == "message":
@@ -24,6 +28,12 @@ def reply(message):
 
       res = current + " price: " + price
       #print(res)
+      
+    elif:
+      if greetings in message["text"]:
+        res = random.choice(greetings_responses) 
+        + " I am low-level bitcoinBot.\n I can ONLY reply current bitcoin price.\n Please ask including 'bitcoin'."
+      
     else: 
-      res = "Sorry, I am low-level bitcoinBot.\n please ask including 'bitcoin'"
+      res = "Sorry, I am low-level bitcoinBot.\n Please ask including 'bitcoin'."
     return res
