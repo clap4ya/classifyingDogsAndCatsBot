4b08781dd8753b2ab5083771f7bc10152a2a843e
1040151 <yeolpyeong@gmail.com>
2017-07-03 15:15:19 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,6 +1,8 @@
 from microsoftbotframework import ReplyToActivity
+import requests
+import json
 
 def echo_response(message):
   if message["type"] == "message":
     ReplyToActivity(fill=message,
-                    text=message["text"]).send()
+                    text=111111111111111111111).send()
