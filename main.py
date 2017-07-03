cf806223d7db4a51567d9d176abe775e5441c5b9
1040151 <yeolpyeong@gmail.com>
2017-07-03 14:03:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,57 @@
-__metaclass__ = type
-
-from microsoftbotframework import MsBot
-from tasks import *
-
 import requests
+
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
 import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
 
 
-bot = MsBot(port=int(os.environ['PORT']))
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
 
-url = 'https://api.korbit.co.kr/v1/ticker'
+    print("Request:")
+    print(json.dumps(req, indent=4))
 
-response = requests.get(url, params=params)
-data = response.json()
+    res = processRequest(req)
 
-def makeReply(data):
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    url = 'https://api.korbit.co.kr/v1/ticker'
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    def makeReply(data):
     price = data['last']
-    return price
+    speech = "price: " + price
+    print(speech)
+    return speech
 
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
 
-bot.add_process(makeReply(data))
-bot.run()
+    print("Starting app on port %d" % port)
+
+app.run(debug=False, port=port, host='0.0.0.0')
