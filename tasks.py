c103f504e06d705b8af8ecc976fc37c81b01a6d7
1040151 <yeolpyeong@gmail.com>
2017-07-03 15:17:41 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -5,4 +5,18 @@ import json
 def echo_response(message):
   if message["type"] == "message":
     ReplyToActivity(fill=message,
-                    text=111111111111111111111).send()
+                    text=replyBitcoin()).send()
+
+def replyBitcoin():
+    url = 'https://api.korbit.co.kr/v1/ticker'
+    params = {
+        'format': json
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    price = data['last']
+    res = "price: " + price
+    print(res)
+    return res
