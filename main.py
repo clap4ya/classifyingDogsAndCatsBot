49378bcb636541fff9af15b5b7efeb630db3e535
1040151 <yeolpyeong@gmail.com>
2017-07-03 13:14:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,69 @@ __metaclass__ = type
 
 from microsoftbotframework import MsBot
 from tasks import *
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import json
 import os
 
+from flask import Flask
+from flask import request
+from flask import make_response
+
+
 bot = MsBot(port=int(os.environ['PORT']))
-bot.add_process(echo_response)
+
+def reply():
+  req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def processRequest(req):
+    baseurl = "https://api.korbit.co.kr/v1/ticker"
+    result = urlopen(baseurl).read()
+    data = json.loads(result)
+    res = makeWebhookResult(data)
+    return res 
+  
+def makeWebhookResult(data):
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    price = channel.get('last')
+
+    speech = "Today: " + price.get('last')
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "lovelybot"
+    }  
+  
+bot.add_process(reply())
 bot.run()
