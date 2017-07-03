514791285b3e811d60c6b8f4ba0011e5d6b07d81
1040151 <yeolpyeong@gmail.com>
2017-07-03 16:21:03 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -5,31 +5,25 @@ import datetime
 
 def response(message):
   if message["type"] == "message":
+    ReplyToActivity(fill=message, text=reply(message)).send()  
+        
+def reply():
     if 'bitcoin' in message["text"]:
-      ReplyToActivity(fill=message,
-                    text=currentBitcoin()).send()  
-    else:
-      ReplyToActivity(fill=message,
-                    text=pardon()).send()  
-    
-def pardon():    
-    ReplyToActivity(fill=message,
-                    text="Sorry, I am bitcoinBot. please ask 'bitcoin' price").send()
-    
-def currentBitcoin():
-    url = 'https://api.korbit.co.kr/v1/ticker'
-    params = {
-        'format': json
-    }
+      url = 'https://api.korbit.co.kr/v1/ticker'es 
+      params = {
+          'format': json
+      }
 
-    response = requests.get(url, params=params)
-    data = response.json()
+      response = requests.get(url, params=params)
+      data = response.json()
 
-    price = data['last']
-    utcnow = datetime.datetime.utcnow()
-    time_gap = datetime.timedelta(hours=9)
-    current = str(utcnow + time_gap)
-    
-    res = current + " price: " + price
-    #print(res)
+      price = data['last']
+      utcnow = datetime.datetime.utcnow()
+      time_gap = datetime.timedelta(hours=9)
+      current = str(utcnow + time_gap)
+
+      res = current + " price: " + price
+      #print(res)
+    else: 
+      res = "Sorry, I am bitcoinBot. please ask 'bitcoin' price"
     return res
