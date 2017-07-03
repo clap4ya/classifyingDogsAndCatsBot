1f9235c66808e16b4d0926887d49ebfbc93b2f28
1040151 <yeolpyeong@gmail.com>
2017-07-03 16:40:47 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -29,7 +29,7 @@ def reply(message):
       res = current + " price: " + price
       #print(res)
       
-    elif message["text"] in greetings
+    elif message["text"] in greetings:
       res = random.choice(greetings_responses) + " I am low-level bitcoinBot.\n I can ONLY reply current bitcoin price.\n Please ask including 'bitcoin'."
       
     else: 
