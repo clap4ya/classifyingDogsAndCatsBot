2b6a9f0232fbbe08572945a0915c2d57e3221ee5
1040151 <yeolpyeong@gmail.com>
2017-07-09 23:31:52 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -3,7 +3,7 @@ import requests
 import json
 import datetime
 import random
-from dataLoader import *
+#from dataLoader import *
 
 greetings = [ "hi", "hello", "hey", "yo", "greetings" ]
 greetings_responses = [ "Hi there." , "Greetings man.", "Hello there.", "Hey." ]
@@ -11,9 +11,7 @@ greetings_responses = [ "Hi there." , "Greetings man.", "Hello there.", "Hey." ]
 def response(message):
   if message["attachments"][0]["contentType"] == "image/jpeg":
     ReplyToActivity(fill=message, text=classify(message)).send()
-  else:
-    ReplyToActivity(fill=message, text="I am low-level classifyingBot. Please send a image.").send()
                             
 def classify(message):
   #data = load_data(message)
-  return "gogogo"
+  return "11111111111111"
