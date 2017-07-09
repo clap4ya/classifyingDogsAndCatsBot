24d50d407b63d788e97599a2266589ea35c9041b
1040151 <yeolpyeong@gmail.com>
2017-07-09 23:43:11 +0900

    Update dataLoader.py

--- dataLoader.py
+++ dataLoader.py
@@ -6,11 +6,11 @@ IMG_SIZE = 50
 
 def load_data(message):
   url = message["attachments"][0]["contentUrl"]
-  img = url_to_img(url)
+  img = url2img(url)
   img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
-  return img
+  return url
 
-def url_to_img(url):
+def url2img(url):
   resp = urlopen(url)
   img = np.asarray(bytearray(resp.read()), dtype="uint8")
   img = cv2.imdecode(img, cv2.IMREAD_GRAYSCALE)
