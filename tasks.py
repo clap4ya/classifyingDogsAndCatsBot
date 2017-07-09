ea6d28e399fc6eeebd037ed9aaee69fefe43c50f
1040151 <yeolpyeong@gmail.com>
2017-07-09 20:55:20 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -8,14 +8,13 @@ greetings = [ "hi", "hello", "hey", "yo", "greetings" ]
 greetings_responses = [ "Hi there." , "Greetings man.", "Hello there.", "Hey." ]
 
 def response(message):
-  if message["type"] == "message":
-    ReplyToActivity(fill=message, text=reply(message)).send()  
-                            
-def reply(message):
-  res = "I am low-level classifyingBot. Please send a image."
-    
   if message["attachments"][0]["contentType"] == "image/jpeg":
-    data = message["attachments"][0]["contentUrl"]
-    res = "ikik"
+    ReplyToActivity(fill=message, text=classify(message)).send()
+  else:
+    ReplyToActivity(fill=message, text="I am low-level classifyingBot. Please send a image.").send()
+                            
+def classify(message):
+  data = message["attachments"][0]["contentUrl"]
+  res = "ikik"
     
   return res
