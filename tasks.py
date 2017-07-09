8033f6f3c40caaf096bb58aadcccc2a639736780
1040151 <yeolpyeong@gmail.com>
2017-07-09 22:55:20 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -3,6 +3,8 @@ import requests
 import json
 import datetime
 import random
+import numpy as np
+from urllib.request import urlopen
 import cv2
 
 greetings = [ "hi", "hello", "hey", "yo", "greetings" ]
