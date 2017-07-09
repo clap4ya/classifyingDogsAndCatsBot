bfed5cf452a42fa760bbd8d9effb06d4b3f446ca
1040151 <yeolpyeong@gmail.com>
2017-07-09 23:54:07 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,17 +1,16 @@
 from microsoftbotframework import ReplyToActivity
-import requests
-import json
-import datetime
-import random
-#from dataLoader import *
-
-greetings = [ "hi", "hello", "hey", "yo", "greetings" ]
-greetings_responses = [ "Hi there." , "Greetings man.", "Hello there.", "Hey." ]
+import cv2
+import numpy as np
+from urllib.request import urlopen
 
 def response(message):
   if message["attachments"][0]["contentType"] == "image/jpeg":
     ReplyToActivity(fill=message, text=classify(message)).send()
                             
 def classify(message):
-  data = message["attachments"][0]["contentUrl"]
-  return data
+  url = message["attachments"][0]["contentUrl"]
+  resp = urlopen(url)
+  img = np.asarray(bytearray(resp.read()), dtype="uint8")
+  img = cv2.imdecode(img, cv2.IMREAD_GRAYSCALE)
+  img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
+  return "111111111111"
