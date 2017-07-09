efd0b97e1712e099d00c1e6de23980942aacfa3d
1040151 <yeolpyeong@gmail.com>
2017-07-09 17:56:41 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -14,24 +14,7 @@ def response(message):
 def reply(message):
     if message["contentType"] == "image/jpeg":
       res = "Success to receive image!!!"
-  
-    if 'bitcoin' in message["text"]:
-      url = 'https://api.korbit.co.kr/v1/ticker'
-      params = {
-          'format': json
-      }
-
-      response = requests.get(url, params=params)
-      data = response.json()
-
-      price = data['last']
-      utcnow = datetime.datetime.utcnow()
-      time_gap = datetime.timedelta(hours=9)
-      current = str(utcnow + time_gap)
-
-      res = current + " price: " + price
-      #print(res)
-      
+    
     elif message["text"] in greetings:
       res = random.choice(greetings_responses) + " I am low-level classifyingBot.\nI can ONLY classify dogs and cats.\nPlease sned a image"
       
