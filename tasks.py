9b7d86bb78f8fb20a1b9479d0b0534b41192a293
1040151 <yeolpyeong@gmail.com>
2017-07-10 22:46:25 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 from microsoftbotframework import ReplyToActivity
 from urllib.request import urlopen
 import numpy as np
-import cv2 
+#import cv2 
 
 
 def response(message):
@@ -10,12 +10,12 @@ def response(message):
                             
 def classify(message):
   url = message["attachments"][0]["contentUrl"]
-  res = url2img(url)
+  #res = url2img(url)
   return "111111111111111111111"
 
-def url2img(url):
-  resp = urlopen(url)
-  img = np.asarray(bytearray(resp.read()), dtype="uint8")
-  img = cv2.imdecode(img, cv2.IMREAD_GRAYSCALE)
-  img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
-  return img
+#def url2img(url):
+  #resp = urlopen(url)
+  #img = np.asarray(bytearray(resp.read()), dtype="uint8")
+  #img = cv2.imdecode(img, cv2.IMREAD_GRAYSCALE)
+  #img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
+  #return img
