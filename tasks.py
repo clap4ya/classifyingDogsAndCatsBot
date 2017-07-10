2a229afd88c35199b81480e3183a707b2d158c2d
1040151 <yeolpyeong@gmail.com>
2017-07-10 18:27:33 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 from microsoftbotframework import ReplyToActivity
 import cv2
 import numpy as np
-from urllib.request import urlopen
+#from urllib.request import urlopen
 
 def response(message):
   if message["attachments"][0]["contentType"] == "image/jpeg":
@@ -9,12 +9,12 @@ def response(message):
                             
 def classify(message):
   url = message["attachments"][0]["contentUrl"]
-  res = url2img(url)
+  #res = url2img(url)
   return url
 
-def url2img(url):
-  resp = urlopen(url)
-  img = np.asarray(bytearray(resp.read()), dtype="uint8")
-  img = cv2.imdecode(img, cv2.IMREAD_GRAYSCALE)
-  img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
-  return img
+#def url2img(url):
+  #resp = urlopen(url)
+  #img = np.asarray(bytearray(resp.read()), dtype="uint8")
+  #img = cv2.imdecode(img, cv2.IMREAD_GRAYSCALE)
+  #img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
+  #return img
