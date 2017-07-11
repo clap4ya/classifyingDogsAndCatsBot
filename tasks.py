6ffaaa9edc2722bbdf5665aa4f015ed154afa13c
1040151 <yeolpyeong@gmail.com>
2017-07-11 13:40:08 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@ from microsoftbotframework import ReplyToActivity
 from urllib.request import urlopen
 import numpy as np
 import cv2 
-import tensorflow as tf
+#import tensorflow as tf
 #from tflearn.layers.conv import conv_2d, max_pool_2d
 #from tflearn.layers.core import input_data, dropout, fully_connected
 #from tflearn.layers.estimator import regression
