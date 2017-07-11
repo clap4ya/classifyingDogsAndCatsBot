ed41620c3c14f1d16194c387a7d5671397f4a5f1
1040151 <yeolpyeong@gmail.com>
2017-07-11 14:40:28 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -23,3 +23,39 @@ def url2img(url):
     img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
     img = np.array(img)
     return img
+
+def load_model():
+    tf.reset_default_graph()
+
+    convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
+
+    convnet = conv_2d(convnet, 32, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 64, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 128, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 64, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+    
+    convnet = conv_2d(convnet, 32, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = fully_connected(convnet, 1024, activation='relu')
+    convnet = dropout(convnet, 0.8)
+
+    convnet = fully_connected(convnet, 2, activation='softmax')
+    convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+    model = tflearn.DNN(convnet, tensorboard_dir='log')
+
+    MODEL_NAME = "dogsvscats-{}-{}.model".format(LR, "2conv-basic")
+  
+    if os.path.exists(MODEL_NAME):
+        model.load(MODEL_NAME)
+        print('model loaded!')
+    
+    return model  
