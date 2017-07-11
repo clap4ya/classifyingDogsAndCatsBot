8f96752e76993f835ccace999d0400752e2472d5
1040151 <yeolpyeong@gmail.com>
2017-07-11 14:56:03 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
 import numpy as np
 import cv2
 import os
-import tensorflow
+import tensorflow as tf
 import tflearn
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.core import input_data, dropout, fully_connected
